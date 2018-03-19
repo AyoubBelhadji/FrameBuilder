dd061a160190ce5102ef98a9d992cbb7827c8d77
AyoubBelhadji <ayoub.belhadji@gmail.com>
2018-03-19 10:31:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'cvxopt', 'sphinxcontrib-bibtex', 'sphinx_rtd_theme'] 
+    install_requires=['numpy', 'itertools', 'scipy.stats', 'scipy.special','matplotlib.pyplot'] 
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
