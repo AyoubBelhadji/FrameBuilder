e975892898ef8131637c8e0df72d819ff343b8d4
AyoubBelhadji <ayoub.belhadji@gmail.com>
2018-03-19 14:32:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'itertools', 'scipy.stats', 'scipy.special','matplotlib.pyplot'] 
+    install_requires=['numpy', 'combinations','binom', 'scipy.special','matplotlib.pyplot'] 
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
