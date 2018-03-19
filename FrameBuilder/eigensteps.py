b5d90007ad4cb98b915586f3dcdf95611c78b803
AyoubBelhadji <ayoub.belhadji@gmail.com>
2018-03-19 11:40:42 +0100

    Update eigensteps.py

--- FrameBuilder/eigensteps.py
+++ FrameBuilder/eigensteps.py
@@ -1,8 +1,7 @@
 def get_eigensteps_random(mu_vector,lambda_vector,N,d):
-    E = np.zeros((N,N)) #(d,N)
+    E = np.zeros((N,N))
     E[:,N-1] = lambda_vector
     for n in range(N-2,-1,-1):
-        #print(n)
         for k in range(n, -1, -1):
             A_n_1_k = max(E[k+1,n+1],np.sum(E[k:n+2,n+1])-np.sum(E[k+1:n+1,n])-mu_vector[n+1])
             B_array = np.zeros(k+1)
@@ -15,10 +14,9 @@ def get_eigensteps_random(mu_vector,lambda_vector,N,d):
     return E
 
 def get_eigensteps_mean(mu_vector,lambda_vector,N,d):
-    E = np.zeros((N,N)) #(d,N)
+    E = np.zeros((N,N))
     E[:,N-1] = lambda_vector
     for n in range(N-2,-1,-1):
-        #print(n)
         for k in range(n, -1, -1):
             A_n_1_k = max(E[k+1,n+1],np.sum(E[k:n+2,n+1])-np.sum(E[k+1:n+1,n])-mu_vector[n+1])
             B_array = np.zeros(k+1)
