aeb8e526f625fd3de978a4d0a21b8e490dbe7588
AyoubBelhadji <ayoub.belhadji@gmail.com>
2018-03-26 10:39:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     #install_requires=['numpy', 'combinations','binom', 'scipy.special','matplotlib.pyplot'] 
-    install_requires=['numpy',  'scipy','matplotlib'] 
+    install_requires=['numpy',  'scipy','matplotlib'] ,
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
